b7145ea94f2879c8639918780de497bd8453fa4e
apee21 <annisaap456@gmail.com>
2022-03-22 02:43:09 -0700

    pertama commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+{
+    echo "<h1>Perkenalkan</h1>";
+    echo "Nama saya Ryomen Sukuna. 
+        Saya merupakan kutukan tingkat khusus 
+        dan sering dipanggil Raja Kutukan";
+}
+}
