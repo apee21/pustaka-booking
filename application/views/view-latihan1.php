cfde1f07be3367577d7736d7835f57960450dad5
apee21 <annisaap456@gmail.com>
2022-04-05 02:53:52 -0700

    kedua commit

---
+++ application/views/view-latihan1.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+    <tittle>Latihan 1</tittle>
+</head>
+
+<body>
+    Hellow ma fren, welcome to ma program!!!!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = ". $hasil; ?>
+
+</body>
+</html>
